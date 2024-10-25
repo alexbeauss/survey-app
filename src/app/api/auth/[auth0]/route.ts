884162ca9665eaa2dbfde285599e35a323097a42
AlexBeaussier <alex.beaussier@gmail.com>
2024-10-25 14:28:49 +0200

    Update route.ts

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,16 +1,8 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
-
-
-export const GET = async (request: Request) => {
-  // Vérifiez si context est défini avant de déstructurer params
-  // const params = context?.params || {}; // Supprimé car non utilisé
-
-  // Call handleAuth to manage authentication
-  return handleAuth({
-    signup: handleLogin({
-      authorizationParams: {
-        screen_hint: 'signup',
-      },
-    }),
-  })(request); // Pass the request to the handler
-};
+export const GET = handleAuth({
+  signup: handleLogin({
+    authorizationParams: {
+      screen_hint: 'signup',
+    },
+  }),
+});
